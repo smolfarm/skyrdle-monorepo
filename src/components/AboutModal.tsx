dd4636d26a94ae2481e793d2b0dc475d15327502
Thorne 🌸 <ExistentialEnso@users.noreply.github.com>
2025-06-25 22:39:07 -0400

    How to play in about

--- src/components/AboutModal.tsx
+++ src/components/AboutModal.tsx
@@ -16,8 +16,15 @@ export default function AboutModal({ onClose }: { onClose: () => void }) {
             <h2>Skyrdle</h2>
             <p>made with &lt;3 by <a href="https://smol.farm" target="_blank" rel="noopener noreferrer">smol farm</a></p>
 
+            <ul style={{listStyleType: 'none', fontSize: '0.8rem'}}>
+                <li>🧐 Guess the word of the day in 6 tries or less!</li>
+                <li>✅ Correct letters & misplaced letters will be highlighted!</li>
+                <li>🏆 Your scores will be saved to your PDS!</li>
+            </ul>
+
             <ul className="modal-list">
                 <li><a href="https://bsky.app/profile/skyrdle.com" target="_blank" rel="noopener noreferrer">follow on bluesky</a></li>
+                <li><a href="https://patreon.com/smolfarm" target="_blank" rel="noopener noreferrer">support on patreon</a></li>
                 <li><a href="https://github.com/smolfarm/skyrdle-monorepo" target="_blank" rel="noopener noreferrer">view source on github</a></li>
             </ul>
             <button className="btn-glass" onClick={() => onClose()}>Close</button>
